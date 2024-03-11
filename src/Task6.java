e58d3bc09cb4daace82509aa9d26edbaec19ccc2
tobi01010010 <rehm.tobias11@gmail.com>
2024-03-11 18:09:01 +0100

    task 6

--- src/Task6.java
+++ src/Task6.java
@@ -1,12 +1,50 @@
 import org.apache.commons.net.telnet.TelnetClient;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class Task6 {
 
     public static void main(String[] args) {
-        String server = "india.colorado.edu";
-        int port = 13; // Standard Telnet-Port
+        //get the time
+        //String server = "india.colorado.edu";
+        //int port = 13; // Standard Telnet-Port
+
+        //Telehack
+        //String server = "telehack.com";
+        //int port = 23;
+
+        //Legend of the Red Dragon
+        //String server = "lord.stabs.org";
+        //int port = 23;
+
+        //BatMUD
+        //String server = "batmud.bat.org";
+        //int port = 23;
+
+
+
+        //Cuban Bar (not working, exit code 130)
+        //String server = "52.88.68.92";
+        //int port = 1234;
+
+        //weather (not working, exit code 130)
+        //String server = "rainmaker.wunderground.com";
+        //int port = 3000;
+
+        //starwars (not working, exit code 130)
+        //String server = "towel.blinkenlights.nl";
+        //int port = 23;
+
+        //Avalon: The Legend Lives (not working, connection refused)
+        //String server = "avalon-rpg.com";
+        //int port = 23;
+
+        //Zombie MUD (not working, exit code 130)
+        //String server = "zombiemud.org";
+        //int port = 23;
+
+
 
         TelnetClient telnetClient = new TelnetClient();
 
@@ -40,4 +78,41 @@ public class Task6 {
         }
     }
 
+
+
+
+/*
+    public static void main starwars(String[] args){
+        //Star Wars asciimation
+        towel.blinkenlights.nl 23
+    }
+
+*/
+
+
+
+
+
+
+/*
+    public static void main(String[] args) {
+        System.out.println("Gib die Adresse des Telnet Servers ein:");
+        System.out.println("Wähle: starwars, gettime oder weather");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        daytime(input);
+*/
+
+
+
+
+
+
+
 }
+
+
+//Freie telnet server; Quelle: https://store.chipkin.com/articles/telnet-list-of-telnet-servers
+//Freie telnet server; Quelle: https://www.telnet.org/htm/places.htm
