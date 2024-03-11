be8c56ef7f66ef5e64709162aa6ee2dd8071bab2
Philipp Voß <83300730+Philipp-Voss@users.noreply.github.com>
2024-03-11 08:15:04 +0100

    NTP Server Daytime

--- src/Task4.java
+++ src/Task4.java
@@ -1,2 +1,41 @@
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
+
+import java.net.InetAddress;
+
+import java.util.Scanner;
+
 public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String ntpServerAddress = scanner.next();
+        NTPUDPClient client = new NTPUDPClient();
+
+        client.setDefaultTimeout(10000);
+        try {
+            client.open();
+            InetAddress hostAddr = InetAddress.getByName(ntpServerAddress);
+            TimeInfo info = client.getTime(hostAddr);
+
+            processTimeInfo(info);
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        } finally {
+            client.close();
+        }
+    }
+
+    private static void processTimeInfo(TimeInfo info) {
+        info.computeDetails();
+
+        if (info.getOffset() != null) {
+            long currentTime = System.currentTimeMillis();
+            long ntpTime = currentTime + info.getOffset();
+            System.out.println("Aktuelle Zeit: " + new java.util.Date(ntpTime));
+        } else {
+            System.out.println("Es konnte keine Zeitverschiebung berechnet werden.");
+        }
+    }
 }
