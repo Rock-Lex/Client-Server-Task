343fb9321fa603369be596e3a3b9bba101fccfef
Oleksandr Kamenskyi <okamenskyi@oleksmac.local>
2024-03-12 14:54:40 +0100

    Task 1 and Task 5

---
+++ src/Task1.java
@@ -0,0 +1,26 @@
+import java.net.*;
+
+/*
+    Ping
+ */
+
+public class Task1 {
+    public static void main(String[] args) {
+        try {
+            InetAddress host = InetAddress.getByName("www.zeit.de");
+            long time = System.nanoTime();
+            int port = 80;
+            Socket sock = new Socket (host, port) ;
+            sock.close();
+            time = (System.nanoTime() - time) / 1000000L;
+            System.out.println("Connection ok (port " + port +", time = " + time + " ms). In" + "Host Address = "
+                    + host.getHostAddress()
+                    + "\n"
+                    + "Host Name = " + host.getHostName());
+            System.exit(0);
+        } catch (Exception ex) {
+                System.out.println("Error: " + ex.getMessage());
+                System.exit(1);
+        }
+    }
+}
--- src/Task5.java
+++ src/Task5.java
@@ -12,5 +12,39 @@ Optional: Einsatz von Apache HTTP Components zur Implementierung
  */
 
 
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Task5 {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("google.com", 80);
+            OutputStream out = socket.getOutputStream();
+            InputStream in = socket.getInputStream();
+
+            // Get Command
+            String s = "GET " + "/" + " HTTP/1.0" + "\r\n\r\n";
+            out.write(s.getBytes());
+
+            int len;
+            byte[] b = new byte[100];
+
+            while ((len = in.read(b)) != -1) {
+                System.out.write(b, 0, len);
+            }
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
