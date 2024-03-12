dadbfe1d5057bc169cf3871596b370b30e98de92
tobi01010010 <rehm.tobias11@gmail.com>
2024-03-12 16:37:45 +0100

    task 6

--- src/Task6.java
+++ src/Task6.java
@@ -1,4 +1,5 @@
 import org.apache.commons.net.telnet.TelnetClient;
+
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
@@ -15,18 +16,15 @@ public class Task6 {
 
     public static void main(String[] args) {
 
-        //Telehack
-//        String server = "telehack.com";
-//        int port = 23;
-
         // Uncomment the following block to allow user input for server selection
         System.out.println("Choose a Telnet server writing the name:");
-        System.out.println("1. starwars");
-        System.out.println("2. gettime");
-        System.out.println("3. telehack");
-        System.out.println("4. lord");
-        System.out.println("5. batmud");
-        System.out.println("6. weather");
+        //System.out.println("starwars");
+        System.out.println("gettime");
+        System.out.println("telehack");
+        System.out.println("lord");
+        System.out.println("batmud");
+        //System.out.println("weather");
+        System.out.println("exit\n");
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
@@ -50,12 +48,15 @@ public class Task6 {
             case "weather":
                 executeTelnet("rainmaker.wunderground.com", 3000);
                 break;
+            case "exit":
+                executeTelnet("india.colorado.edu", 13);
+                break;
             default:
                 System.out.println("Invalid choice.");
         }
 
         // Uncomment the above block and comment out the line below if you want to prompt the user for server selection
-//        executeTelnet("india.colorado.edu", 13); // Default option if user input is not used
+        // executeTelnet("india.colorado.edu", 13); // Default option if user input is not used
     }
 
     private static void executeTelnet(String server, int port) {
@@ -94,7 +95,7 @@ public class Task6 {
 
 
 
-// Notizen
+// Funktionierende Telnet-Server
 //get the time
 //String server = "india.colorado.edu";
 //int port = 13; // Standard Telnet-Port
@@ -112,7 +113,7 @@ public class Task6 {
 //int port = 23;
 
 
-
+//nicht ganz funktionierende Telnetserver
 //Cuban Bar (not working, exit code 130)
 //String server = "52.88.68.92";
 //int port = 1234;
--- src/Task7.java
+++ src/Task7.java
@@ -6,7 +6,7 @@ Serves mit Hilfe der Klasse ServerSocket.
 - Verwendung der Klasse
 - Angebotene Methoden
 Implementieren Sie eine einfache Serveranwendung die mit einem Telnet-Client (arbeitet oberhalb von TCP/IP) kommunizieren kann.
-Testen Sie die Anwendung zuerst indem Client und Server (localhost <portNumber>)aufeinemSystemarbeiteten,dann aber auch im im lokalen Netzwerkbereich.
+Testen Sie die Anwendung zuerst indem Client und Server (localhost <portNumber>)auf einem System arbeiteten, dann aber auch im lokalen Netzwerkbereich.
 Bem.: Telnet steht über die Kommandozeile zur Verfügung und erlaubt eine zeichenweise Übertragung zwischen Client und Server.
 Optional: Einsatz von Apache Commons Net für den Telnet-Client
  */
